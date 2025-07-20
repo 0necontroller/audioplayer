932cee280ce42f8c11b2f37e6f943f345a7e09a2
Abel Misiocha <wells.mail.13@gmail.com>
2025-07-20 11:48:19 +0300

    Implement code changes to enhance functionality and improve performance

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 	return (
 		<>
 			<MyCookieConsent />
-			<main className="flex min-h-screen flex-col items-center justify-center space-y-8 p-24">
+			<main className="flex min-h-screen flex-col items-center justify-center space-y-8">
 				<h1 className="mb-8 text-center text-4xl font-bold">
 					Audio Player Demo
 				</h1>
